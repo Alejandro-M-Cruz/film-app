6830905b8b058f7ea9d4eb8c4fcf7d0cf40aa91f
Alejandro Cruz <alejandro.matias@secret-source.eu>
2025-02-27 00:04:28 +0000

    Added Date util

--- utils/date.go
+++ utils/date.go
@@ -1,33 +1,54 @@
 package utils
 
 import (
-	"encoding/json"
-	"time"
+    "database/sql/driver"
+    "encoding/json"
+    "errors"
+    "time"
 )
 
 type Date struct {
-	time.Time
+    time.Time
 }
 
 func NewDate(date time.Time) Date {
-	return Date{date}
+    return Date{date}
+}
+
+func ParseDate(value string) (Date, error) {
+    t, err := time.Parse(time.DateOnly, value)
+    return NewDate(t), err
+}
+
+func (d *Date) Scan(value any) error {
+    date, ok := value.(time.Time)
+    if !ok {
+        return errors.New("date must be a time.Time")
+    }
+
+    d.Time = date
+    return nil
+}
+
+func (d Date) Value() (driver.Value, error) {
+    return d.Time, nil
 }
 
 func (d *Date) UnmarshalJSON(b []byte) error {
-	var dateStr string
-	if err := json.Unmarshal(b, &dateStr); err != nil {
-		return err
-	}
+    var dateStr string
+    if err := json.Unmarshal(b, &dateStr); err != nil {
+        return err
+    }
 
-	date, err := time.Parse(time.DateOnly, dateStr)
-	if err != nil {
-		return err
-	}
+    date, err := ParseDate(dateStr)
+    if err != nil {
+        return err
+    }
 
-	d.Time = date
-	return nil
+    *d = date
+    return err
 }
 
 func (d Date) MarshalJSON() ([]byte, error) {
-	return json.Marshal(d.Time.Format(time.DateOnly))
+    return json.Marshal(d.Time.Format(time.DateOnly))
 }
